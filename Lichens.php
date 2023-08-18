3e8522e2a8f38d4a40c9143da32a5ef7924319f6
muner12 <bakhtmuner06@gmail.com>
2023-08-18 21:14:30 +0500

    Lichens images issues fixing

--- Lichens.php
+++ Lichens.php
@@ -236,10 +236,11 @@ if($result==1){
                     <?php
                     if(isset($_GET['type']) && $_GET['type']=='update'){
                         echo "<img width='100px' height='60px' src='upload/lichens/{$fetch_one_row['picture']}'>";
-                    }
+                    
+                    
                     ?>
                     <span class="text-danger" style="font-size:10px;">Note:If You not select Image, by default this picture wil be selected<span>
-
+                    <?php } ?>
                         </div>
             </div>
             
