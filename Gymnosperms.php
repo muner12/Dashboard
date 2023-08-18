e6f200d46a6918cf615f40f49310d513992cb16a
sarzamin shah <sarzaminkha101@gmail.com>
2023-08-18 13:05:59 +0500

    changes

--- Gymnosperms.php
+++ Gymnosperms.php
@@ -31,7 +31,7 @@ if(isset($_POST['insert'])){
    
     $sql="INSERT INTO `gymnosperms` ( `botanical`, `synonyams`, `family`, `group`, `country`, `province`, `collector`, `collection_number`, `year`, `upload_date`, `description`, `picture`) 
     VALUES ('$botincal', '$sysnonyams', '$family', '$group', '$country', '$province', '$collector', '$collection_number', '$year', '$upload_date', '$desciption', '$picture_name')";
-    move_uploaded_file($picture_tempname,"upload/Gymnosperm/$picture_name");
+    move_uploaded_file($picture_tempname,"upload/Gymnosperms/$picture_name");
     $result=mysqli_query($con,$sql);
 
 if($result==1){
