6b27edb504522e98cb2cbb830a6c5cb07692173d
muner12 <bakhtmuner06@gmail.com>
2023-10-05 12:48:28 +0500

    profail

--- Profile.php
+++ Profile.php
@@ -3,10 +3,16 @@
 include('header.php');
 ?>
 
-<div class=>
+<div class=" d-flex justify-content-center">
 
-
-
+    <div class="shadow-lg p-3 mb-5 bg-white rounded">
+    <form>
+                    <label class="custom-file-upload-circle">
+                        <input type="file" class="custom-file-input">
+                        <span class="custom-file-label">Choose File</span>
+                    </label>
+                </form>
+    </div>
 </div>
 
 
