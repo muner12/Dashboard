bc30e063363f57aea451453a92862e575cdd2527
Rahman Ullah <rahmanbunerwal91@gmail.com>
2023-10-06 19:58:13 +0500

    Rahman changes

--- admin/footer.php
+++ admin/footer.php
@@ -91,7 +91,7 @@
           <div class="col-md-7 col-lg-8 text-center text-md-start">
             <!-- Copyright -->
             <div class="p-3">
-              © 2023 UOB Herbarium Catalog Collection.All Rights Reserved:
+              © 2023 UOB Herbarium Catalog Collection.All Rights Reserved
               <a class="text-white" href="https://mdbootstrap.com/"></a>
             </div>
             <!-- Copyright -->
